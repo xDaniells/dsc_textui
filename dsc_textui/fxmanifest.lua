1f8a3f79007bf38a27cc9d60d23ba870704cb7df
iDannx <123790844+xDaniells@users.noreply.github.com>
2023-05-18 12:41:04 -0500

    Add files via upload

---
+++ dsc_textui/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game "gta5"
+
+description "DSC Company CO"
+
+client_scripts {
+    "*.lua"
+}
+
+ui_page {
+    "ui/index.html"
+}
+
+files {
+    "ui/*.html",
+    "ui/*.js",
+    "ui/*.css",
+    "ui/fonts/*.ttf",
+}
